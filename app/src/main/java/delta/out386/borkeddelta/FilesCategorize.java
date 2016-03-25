97c6243b87fbc1a6597fb2d0acd3f9d2b18802e1
Ritayan Chakraborty <ritayanout@gmail.com>
2016-03-25 15:55:30 +0530

    Rename FilesCatagorize

---
+++ app/src/main/java/delta/out386/borkeddelta/FilesCategorize.java
@@ -0,0 +1,52 @@
+package delta.out386.borkeddelta;
+
+import android.view.View;
+import android.content.Context;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+/**
+ * Created by J-PC on 3/19/2016.
+ */
+public class FilesCategorize {
+    View rootView;
+    List<File> zips;
+	Context context;
+
+
+    public List<Flashables> run(Collection<File> fileCollection, View rootView, Context context) {
+        this.rootView = rootView;
+		this.context = context;
+        zips=(List<File>) fileCollection;
+        return sortSize();
+
+    }
+
+    public List<Flashables> sortSize(){
+		if(zips == null)
+			return null;
+		SortFileType sortFile = new SortFileType(context);
+        FlashablesTypeList flashablesTypeList = new FlashablesTypeList();
+		String zipType;
+        long size=0;
+		
+        for(File current:zips){
+            size=FileUtils.sizeOf(current);
+            if(size < 555745280)
+                if((zipType=sortFile.sort(current)) != null)
+                	flashablesTypeList.addFlashable(new Flashables(current, zipType, size));
+
+        }
+        return flashablesTypeList.roms;
+        /*for(FlashablesTypeList current:flashablesArray)
+            if(current != null)
+				if(! current.type.equals("noFlash"))
+                	    output.add(current);
+		return output;*/
+    }
+}
