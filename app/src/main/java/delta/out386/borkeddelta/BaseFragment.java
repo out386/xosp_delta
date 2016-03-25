3fec8ad244bfc21edb1bfb795fc6ea5359d918fb
Ritayan Chakraborty <ritayanout@gmail.com>
2016-03-25 15:26:28 +0530

    Saperate SearchZips from BaseFragment

--- app/src/main/java/delta/out386/borkeddelta/BaseFragment.java
+++ app/src/main/java/delta/out386/borkeddelta/BaseFragment.java
@@ -62,50 +62,6 @@ public class BaseFragment extends Fragment {
         return rootView;
     }
 
-    private class SearchZips extends AsyncTask<Void, Void,List<Flashables> >{
 
-        Context context;
-        View rootView;
-		LoadingDialogFragment loading = new LoadingDialogFragment();
-        public SearchZips(Context cont,View rootView){
-            this.rootView = rootView;
-            context=cont;
-        }
-
-        @Override
-        protected void onPreExecute(){
-            /*TextView outputTV = (TextView) rootView.findViewById(R.id.textView);
-            outputTV.setText("working");*/
-
-			loading.setCancelable(false);
-			loading.show(getFragmentManager(),"dialog");
-        }
-        @Override
-        protected List<Flashables> doInBackground(Void... params){
-            File directory = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/UCDownloads");
-            Collection zipsCollection = FileUtils.listFiles(directory, new String [] {"zip"}, false);
-			return new FilesCatagorize().run(zipsCollection, rootView, context);
-           // return zipsCollection;
-        }
-
-        @Override
-        protected void onPostExecute(List<Flashables> output){
-			/*TextView out = (TextView) rootView.findViewById(R.id.textView);
-			out.setText(output);*/
-            ListView lv=(ListView) rootView.findViewById(R.id.listView);
-            FlashablesAdapter adapter = new FlashablesAdapter(context,
-                    R.layout.list_item, output);
-
-            // Assign adapter to ListView
-            lv.setAdapter(
-                    new SlideExpandableListAdapter(
-                            adapter,
-                            R.id.list_normal_view,
-                            R.id.list_expandable_view)
-            );
-
-            loading.dismiss();
-        }
-    }
 
 }
