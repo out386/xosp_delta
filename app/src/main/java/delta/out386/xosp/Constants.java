dadi11/ThugOTA

Dispaly proper messages
...if :
() The delta file is missing
() The directory is empty

Looked like an app bug earlier.

app/src/main/java/delta/out386/thugota/Constants.java
@@ -8,7 +8,8 @@ public class Constants {
     final static String GENERIC_DIALOG = "delta.out386.thugota.GENERIC_DIALOG";
     final static String PROGRESS_DIALOG = "delta.out386.thugota.PROGRESS_DIALOG";
     final static String ACTION_NOT_ROM_DIALOG = "delta.out386.thugota.NOT_ROM_DIALOG";
-    final static String AUTO_UPDATE = "delta.out386.thugota.AUTO_UPDATE_DIALOG";
+    final static String AUTO_UPDATE = "delta.out386.thugota.AUTO_UPDATE";
+    final static String NO_ROMS = "delta.out386.thugota.NO_ROMS";
 
     final static String PROGRESS = "delta.out386.thugota.PROGRESS";
     final static String DIALOG_MESSAGE = "delta.out386.thugota.DIALOG_MESSAGE";

out386/xosp_delta

app/src/main/java/delta/out386/xosp/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
     final static String PROGRESS_DIALOG = "delta.out386.xosp.PROGRESS_DIALOG";
     final static String ACTION_NOT_XOSP_DIALOG = "delta.out386.xosp.NOT_XOSP_DIALOG";
 	final static String AUTO_UPDATE = "delta.out386.xosp.AUTO_UPDATE_DIALOG";
+	final static String NO_ROMS = "delta.out386.xosp.NO_ROMS";
 	
     final static String PROGRESS = "delta.out386.xosp.PROGRESS";
     final static String DIALOG_MESSAGE = "delta.out386.xosp.DIALOG_MESSAGE";
@@ -18,6 +19,8 @@ public class Constants {
 
     /**
      * Information about the supported rom.
+	 
+	 
      * SUPPORTED_ROM_PROP is the is the property that XOSP uses to identify itself.
      * SUPPORTED_ROM_PROP_NAME is any unique part of the SUPPORTED_ROM_PROP property.
      */
@@ -28,7 +31,7 @@ public class Constants {
 	
 	// The delimiter(s) used in the ROM zip to separate name, date, version, etc. 
     // Include "." in delimiter, adjust LOCATION constants appropiately
-    final static String ROM_ZIP_DELIMITER = "-_.";
+    final static String ROM_ZIP_DELIMITER = "-.";
     final static int ROM_ZIP_NAME_LOCATION = 1;
     final static int ROM_ZIP_DATE_LOCATION = 5;
     final static int ROM_ZIP_DEVICE_LOCATION = 6;