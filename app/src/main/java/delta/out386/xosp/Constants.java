dadi11/ThugOTA

Fix the intent bugs
The bug was caused due to relying on random elements (the 90ms Thread.sleep that was removed), and bad practice.

app/src/main/java/delta/out386/thugota/Constants.java
@@ -22,7 +22,9 @@ public class Constants {
 
     final static String ACTION_CLOSE_DIALOG = "delta.out386.thugota.CLOSE_DIALOG";
     final static String ACTION_APPLY_DIALOG = "delta.out386.thugota.APPLY_DIALOG";
+    final static String ACTION_APPLY_DIALOG_FIRST_START = "delta.out386.thugota.APPLY_DIALOG_FIRST_START";
     final static String GENERIC_DIALOG = "delta.out386.thugota.GENERIC_DIALOG";
+    final static String GENERIC_DIALOG_FIRST_START = "delta.out386.thugota.GENERIC_DIALOG_FIRST_START";
     final static String PROGRESS_DIALOG = "delta.out386.thugota.PROGRESS_DIALOG";
     final static String ACTION_NOT_ROM_DIALOG = "delta.out386.thugota.NOT_ROM_DIALOG";
     final static String AUTO_UPDATE = "delta.out386.thugota.AUTO_UPDATE";

app/src/main/java/delta/out386/thugota/DeltaDialogActivity.java
@@ -27,6 +27,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
@@ -62,42 +63,6 @@ public class DeltaDialogActivity extends Activity {
             progressbar.setProgress(progress);
         }
     };
-    BroadcastReceiver genericMessageReciever = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            AVLoadingIndicatorView loader = (AVLoadingIndicatorView)findViewById(R.id.aviLoader);
-            RelativeLayout okButton = (RelativeLayout)findViewById(R.id.ok_button);
-            String text = intent.getStringExtra(Constants.GENERIC_DIALOG_MESSAGE);
-            loader.setVisibility(View.GONE);
-            okButton.setVisibility(View.VISIBLE);
-            allowBack = true;
-            okButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    onBackPressed();
-                }
-            });
-            loadingText.setText(text);
-        }
-    };
-    
-    BroadcastReceiver notRomReciever = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            AVLoadingIndicatorView loader = (AVLoadingIndicatorView)findViewById(R.id.aviLoader);
-            RelativeLayout okButton = (RelativeLayout)findViewById(R.id.ok_button);
-            String text = "Sorry. This app only works on " + Constants.SUPPORTED_ROM_FULL_NAME;
-            loader.setVisibility(View.GONE);
-            okButton.setVisibility(View.VISIBLE);
-            okButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    finish();
-                }
-            });
-            loadingText.setText(text);
-        }
-    };
 
     @Override
     protected void onResume() {
@@ -112,14 +77,6 @@ public class DeltaDialogActivity extends Activity {
         IntentFilter close = new IntentFilter();
         close.addAction(Constants.ACTION_CLOSE_DIALOG);
         LocalBroadcastManager.getInstance(getApplication()).registerReceiver(closeReciever, close);
-
-        IntentFilter genericMessage = new IntentFilter();
-        genericMessage.addAction(Constants.GENERIC_DIALOG);
-        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(genericMessageReciever, genericMessage);
-
-        IntentFilter notRom = new IntentFilter();
-        notRom.addAction(Constants.ACTION_NOT_ROM_DIALOG);
-        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(notRomReciever, notRom);
         super.onResume();
     }
     @Override
@@ -131,6 +88,7 @@ public class DeltaDialogActivity extends Activity {
         loadingText = (TextView)findViewById(R.id.loadingText);
         loadingText.setText("Working");
         progressbar = (NumberProgressBar)findViewById(R.id.progressbar);
+        intentCheck();
     }
     public void finish() {
 
@@ -141,13 +99,58 @@ public class DeltaDialogActivity extends Activity {
         LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(closeReciever);
         LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(applyReciever);
         LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(progressReciever);
-        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(genericMessageReciever);
-        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(notRomReciever);
         super.onPause();
     }
     @Override
     public void onBackPressed() {
         if(allowBack)
             super.onBackPressed();
+    }
+
+    private void intentCheck() {
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        if(action == null)
+            return;
+        if(action.equals(Constants.GENERIC_DIALOG))
+            genericDialog(intent);
+        else if(action.equals(Constants.ACTION_NOT_ROM_DIALOG))
+            notRomDialog();
+        else if(action.equals(Constants.ACTION_APPLY_DIALOG))
+            applyDialog(intent);
+    }
+    private void genericDialog(Intent intent) {
+        AVLoadingIndicatorView loader = (AVLoadingIndicatorView)findViewById(R.id.aviLoader);
+        RelativeLayout okButton = (RelativeLayout)findViewById(R.id.ok_button);
+        String text = intent.getStringExtra(Constants.GENERIC_DIALOG_MESSAGE);
+        loader.setVisibility(View.GONE);
+        okButton.setVisibility(View.VISIBLE);
+        allowBack = true;
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        loadingText.setText(text);
+    }
+    private void notRomDialog() {
+        AVLoadingIndicatorView loader = (AVLoadingIndicatorView)findViewById(R.id.aviLoader);
+        RelativeLayout okButton = (RelativeLayout)findViewById(R.id.ok_button);
+        String text = "Sorry. This app only works on " + Constants.SUPPORTED_ROM_FULL_NAME;
+        loader.setVisibility(View.GONE);
+        okButton.setVisibility(View.VISIBLE);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        loadingText.setText(text);
+    }
+    private void applyDialog(Intent intent) {
+        Log.v("ThugOTA", "RECEIVED DELTA");
+        String text = intent.getStringExtra(Constants.DIALOG_MESSAGE);
+        loadingText.setText(text);
     }
 }

app/src/main/java/delta/out386/thugota/MainActivity.java
@@ -23,11 +23,14 @@ package delta.out386.thugota;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.content.Intent;
@@ -49,6 +52,37 @@ public class MainActivity extends Activity
     private ListView mDrawerList;
     private String [] mDrawerItems;
     private Toolbar toolbar;
+
+    BroadcastReceiver applyReciever = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.v("ThugOTA", "RECEIVED MAIN");
+            startActivity(new Intent(getApplicationContext(), DeltaDialogActivity.class)
+                    .setAction(Constants.ACTION_APPLY_DIALOG)
+                    .putExtra(Constants.DIALOG_MESSAGE, "Extracting the delta"));
+        }
+    };
+    BroadcastReceiver genericMessageReciever = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.v("ThugOTA", "RECEIVED MAIN");
+            String message = intent.getStringExtra(Constants.GENERIC_DIALOG_MESSAGE);
+            startActivity(new Intent(getApplicationContext(), DeltaDialogActivity.class)
+                    .setAction(Constants.GENERIC_DIALOG)
+                    .putExtra(Constants.GENERIC_DIALOG_MESSAGE, message));
+        }
+    };
+    @Override
+    protected void onResume() {
+        IntentFilter apply = new IntentFilter();
+        apply.addAction(Constants.ACTION_APPLY_DIALOG_FIRST_START);
+        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(applyReciever, apply);
+
+        IntentFilter genericMessage = new IntentFilter();
+        apply.addAction(Constants.ACTION_APPLY_DIALOG_FIRST_START);
+        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(genericMessageReciever, genericMessage);
+        super.onResume();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState)
 	{
@@ -73,7 +107,8 @@ public class MainActivity extends Activity
             List<String> romVersion = Shell.SH.run("getprop " + Constants.SUPPORTED_ROM_PROP);
             if (romVersion == null || romVersion.size() == 0 || !romVersion.get(0).contains(Constants.SUPPORTED_ROM_PROP_NAME)) {
                 // Unsupported ROM. Let's quit.
-                new NotSupportedRom().execute();
+                startActivity(new Intent(getApplication(), DeltaDialogActivity.class).setAction(Constants.ACTION_NOT_ROM_DIALOG));
+                finish();
             }
         }
         FragmentManager fragmentManager = getFragmentManager();
@@ -96,24 +131,7 @@ public class MainActivity extends Activity
                     .commit();*/
         }
     }
-    private class NotSupportedRom extends AsyncTask<Void, Void, Void>
-    {
-        @Override
-        protected Void doInBackground(Void... voids) {
-            Intent notRomDialog = new Intent(Constants.ACTION_NOT_ROM_DIALOG);
-            startActivity(new Intent(getApplication(), DeltaDialogActivity.class)
-                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-            try {
-                Thread.sleep(4000);
-                // Delay needed as the activity needs time to register the receiver
-            }
-            catch (InterruptedException e)
-            {}
-            LocalBroadcastManager.getInstance(getApplication()).sendBroadcast(notRomDialog);
-            finish();
-            return null;
-        }
-    }
+
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView parent, View view, int position, long id) {
@@ -179,5 +197,11 @@ public class MainActivity extends Activity
             }
             return v;
         }
+    }
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(applyReciever);
+        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(genericMessageReciever);
+        super.onPause();
     }
 }

out386/xosp_delta

app/src/main/java/delta/out386/xosp/Constants.java
@@ -22,7 +22,9 @@ public class Constants {
 
     final static String ACTION_CLOSE_DIALOG = "delta.out386.xosp.CLOSE_DIALOG";
     final static String ACTION_APPLY_DIALOG = "delta.out386.xosp.APPLY_DIALOG";
+	final static String ACTION_APPLY_DIALOG_FIRST_START = "delta.out386.xosp.APPLY_DIALOG_FIRST_START";
     final static String GENERIC_DIALOG = "delta.out386.xosp.GENERIC_DIALOG";
+	final static String GENERIC_DIALOG_FIRST_START = "delta.out386.xosp.GENERIC_DIALOG_FIRST_START";
     final static String PROGRESS_DIALOG = "delta.out386.xosp.PROGRESS_DIALOG";
     final static String ACTION_NOT_XOSP_DIALOG = "delta.out386.xosp.NOT_XOSP_DIALOG";
 	final static String AUTO_UPDATE = "delta.out386.xosp.AUTO_UPDATE_DIALOG";
@@ -45,7 +47,7 @@ public class Constants {
     final static String SUPPORTED_ROM_PROP_NAME="XOSP";
 	
 	// The delimiter(s) used in the ROM zip to separate name, date, version, etc. 
-    // Include "." in delimiter, adjust LOCATION constants appropiately
+    // Include "." in delimiter, adjust LOCATION constants appropriately
     final static String ROM_ZIP_DELIMITER = "-.";
     final static int ROM_ZIP_NAME_LOCATION = 1;
     final static int ROM_ZIP_DATE_LOCATION = 5;

app/src/main/java/delta/out386/xosp/DeltaDialogActivity.java
@@ -26,6 +26,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
@@ -62,44 +63,7 @@ public class DeltaDialogActivity extends Activity {
             progressbar.setProgress(progress);
         }
     };
-    BroadcastReceiver genericMessageReciever = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            AVLoadingIndicatorView loader = (AVLoadingIndicatorView)findViewById(R.id.aviLoader);
-            RelativeLayout okButton = (RelativeLayout)findViewById(R.id.ok_button);
-            progressbar.setVisibility(View.GONE);
-            String text = intent.getStringExtra(Constants.GENERIC_DIALOG_MESSAGE);
-            loader.setVisibility(View.GONE);
-            okButton.setVisibility(View.VISIBLE);
-            allowBack = true;
-            okButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    onBackPressed();
-                }
-            });
-            loadingText.setText(text);
-        }
-    };
-
-    BroadcastReceiver notXospReciever = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            AVLoadingIndicatorView loader = (AVLoadingIndicatorView)findViewById(R.id.aviLoader);
-            RelativeLayout okButton = (RelativeLayout)findViewById(R.id.ok_button);
-            String text = "Sorry. This app only works on XOSP";
-            loader.setVisibility(View.GONE);
-            okButton.setVisibility(View.VISIBLE);
-            okButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    finish();
-                }
-            });
-            loadingText.setText(text);
-        }
-    };
-
+	
     @Override
     protected void onResume() {
         IntentFilter apply = new IntentFilter();
@@ -113,14 +77,6 @@ public class DeltaDialogActivity extends Activity {
         IntentFilter close = new IntentFilter();
         close.addAction(Constants.ACTION_CLOSE_DIALOG);
         LocalBroadcastManager.getInstance(getApplication()).registerReceiver(closeReciever, close);
-
-        IntentFilter genericMessage = new IntentFilter();
-        genericMessage.addAction(Constants.GENERIC_DIALOG);
-        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(genericMessageReciever, genericMessage);
-
-        IntentFilter notXosp = new IntentFilter();
-        notXosp.addAction(Constants.ACTION_NOT_XOSP_DIALOG);
-        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(notXospReciever, notXosp);
         super.onResume();
     }
     @Override
@@ -132,6 +88,7 @@ public class DeltaDialogActivity extends Activity {
         loadingText = (TextView)findViewById(R.id.loadingText);
         loadingText.setText("Working");
         progressbar = (NumberProgressBar)findViewById(R.id.progressbar);
+        intentCheck();
     }
     public void finish() {
 
@@ -142,13 +99,58 @@ public class DeltaDialogActivity extends Activity {
         LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(closeReciever);
         LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(applyReciever);
         LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(progressReciever);
-        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(genericMessageReciever);
-        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(notXospReciever);
         super.onPause();
     }
     @Override
     public void onBackPressed() {
         if(allowBack)
             super.onBackPressed();
+    }
+
+    private void intentCheck() {
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        if(action == null)
+            return;
+        if(action.equals(Constants.GENERIC_DIALOG))
+            genericDialog(intent);
+        else if(action.equals(Constants.ACTION_NOT_XOSP_DIALOG))
+            notRomDialog();
+        else if(action.equals(Constants.ACTION_APPLY_DIALOG))
+            applyDialog(intent);
+    }
+    private void genericDialog(Intent intent) {
+        AVLoadingIndicatorView loader = (AVLoadingIndicatorView)findViewById(R.id.aviLoader);
+        RelativeLayout okButton = (RelativeLayout)findViewById(R.id.ok_button);
+        String text = intent.getStringExtra(Constants.GENERIC_DIALOG_MESSAGE);
+        loader.setVisibility(View.GONE);
+        okButton.setVisibility(View.VISIBLE);
+        allowBack = true;
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        loadingText.setText(text);
+    }
+    private void notRomDialog() {
+        AVLoadingIndicatorView loader = (AVLoadingIndicatorView)findViewById(R.id.aviLoader);
+        RelativeLayout okButton = (RelativeLayout)findViewById(R.id.ok_button);
+        String text = "Sorry. This app only works on " + Constants.SUPPORTED_ROM_FULL_NAME;
+        loader.setVisibility(View.GONE);
+        okButton.setVisibility(View.VISIBLE);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        loadingText.setText(text);
+    }
+    private void applyDialog(Intent intent) {
+        Log.v("ThugOTA", "RECEIVED DELTA");
+        String text = intent.getStringExtra(Constants.DIALOG_MESSAGE);
+        loadingText.setText(text);
     }
 }

app/src/main/java/delta/out386/xosp/MainActivity.java
@@ -22,11 +22,14 @@ package delta.out386.xosp;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.content.Intent;
@@ -48,6 +51,37 @@ public class MainActivity extends Activity
     private ListView mDrawerList;
     private String [] mDrawerItems;
     private Toolbar toolbar;
+
+    BroadcastReceiver applyReciever = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.v("ThugOTA", "RECEIVED MAIN");
+            startActivity(new Intent(getApplicationContext(), DeltaDialogActivity.class)
+                    .setAction(Constants.ACTION_APPLY_DIALOG)
+                    .putExtra(Constants.DIALOG_MESSAGE, "Extracting the delta"));
+        }
+    };
+    BroadcastReceiver genericMessageReciever = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.v("ThugOTA", "RECEIVED MAIN");
+            String message = intent.getStringExtra(Constants.GENERIC_DIALOG_MESSAGE);
+            startActivity(new Intent(getApplicationContext(), DeltaDialogActivity.class)
+                    .setAction(Constants.GENERIC_DIALOG)
+                    .putExtra(Constants.GENERIC_DIALOG_MESSAGE, message));
+        }
+    };
+    @Override
+    protected void onResume() {
+        IntentFilter apply = new IntentFilter();
+        apply.addAction(Constants.ACTION_APPLY_DIALOG_FIRST_START);
+        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(applyReciever, apply);
+
+        IntentFilter genericMessage = new IntentFilter();
+        apply.addAction(Constants.ACTION_APPLY_DIALOG_FIRST_START);
+        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(genericMessageReciever, genericMessage);
+        super.onResume();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState)
 	{
@@ -66,13 +100,12 @@ public class MainActivity extends Activity
         // Set the list's click listener
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
-
-        // Checking if the app has been made ROM specific or not
         if(Constants.SUPPORTED_ROM_PROP != null) {
             List<String> romVersion = Shell.SH.run("getprop " + Constants.SUPPORTED_ROM_PROP);
             if (romVersion == null || romVersion.size() == 0 || !romVersion.get(0).contains(Constants.SUPPORTED_ROM_PROP_NAME)) {
                 // Unsupported ROM. Let's quit.
-                new NotSupportedRom().execute();
+                startActivity(new Intent(getApplication(), DeltaDialogActivity.class).setAction(Constants.ACTION_NOT_XOSP_DIALOG));
+                finish();
             }
         }
         FragmentManager fragmentManager = getFragmentManager();
@@ -95,24 +128,7 @@ public class MainActivity extends Activity
                     .commit();*/
         }
     }
-    private class NotSupportedRom extends AsyncTask<Void, Void, Void>
-    {
-        @Override
-        protected Void doInBackground(Void... voids) {
-            Intent notXospDialog = new Intent(Constants.ACTION_NOT_XOSP_DIALOG);
-            startActivity(new Intent(getApplication(), DeltaDialogActivity.class)
-                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-            try {
-                Thread.sleep(4000);
-                // Delay needed as the activity needs time to register the receiver
-            }
-            catch (InterruptedException e)
-            {}
-            LocalBroadcastManager.getInstance(getApplication()).sendBroadcast(notXospDialog);
-            finish();
-            return null;
-        }
-    }
+	
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView parent, View view, int position, long id) {
@@ -178,5 +194,11 @@ public class MainActivity extends Activity
             }
             return v;
         }
+    }
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(applyReciever);
+        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(genericMessageReciever);
+        super.onPause();
     }
 }