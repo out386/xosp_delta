dadi11/ThugOTA

Get the device codename through a prop

app/src/main/java/delta/out386/thugota/Constants.java
@@ -16,6 +16,8 @@
  */
 
 package delta.out386.thugota;
+import eu.chainfire.libsuperuser.Shell;
+
 
 public class Constants {
     final static String TAG = "ThugOTA";
@@ -55,7 +57,7 @@ public class Constants {
     final static int ROM_ZIP_DATE_LOCATION = 6;
     final static int ROM_ZIP_DEVICE_LOCATION = 7;
     final static String ROM_ZIP_NAME = "ResurrectionRemix";
-    final static String ROM_ZIP_DEVICE_NAME = "lettuce";
+    final static String ROM_ZIP_DEVICE_NAME = Shell.SH.run("getprop ro.rr.device").get(0);
     /**
      * EXAMPLE : ROMName-VersionMajor.VersionMinor-Device-Date.zip
      */

out386/xosp_delta

app/src/main/java/delta/out386/xosp/Constants.java
@@ -16,6 +16,7 @@
  */
  
 package delta.out386.xosp;
+import eu.chainfire.libsuperuser.Shell;
 
 public class Constants {
     final static String TAG = "XOSPDelta";
@@ -53,7 +54,7 @@ public class Constants {
     final static int ROM_ZIP_DATE_LOCATION = 5;
     final static int ROM_ZIP_DEVICE_LOCATION = 6;
     final static String ROM_ZIP_NAME = "XOSP";
-    final static String ROM_ZIP_DEVICE_NAME = "Z00A";
+    final static String ROM_ZIP_DEVICE_NAME = Shell.SH.run("getprop ro.xosp.device").get(0);
     /**
      * EXAMPLE : ROMName-VersionMajor.VersionMinor-OFFICIAL-Date-Device.zip
      */