dadi11/ThugOTA

Use LocalBroadcastManager

app/src/main/java/delta/out386/thugota/DeltaDialogActivity.java
@@ -26,6 +26,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
@@ -102,23 +103,23 @@ public class DeltaDialogActivity extends Activity {
     protected void onResume() {
         IntentFilter apply = new IntentFilter();
         apply.addAction(Constants.ACTION_APPLY_DIALOG);
-        registerReceiver(applyReciever, apply);
+        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(applyReciever, apply);
 
         IntentFilter progress = new IntentFilter();
         progress.addAction(Constants.PROGRESS_DIALOG);
-        registerReceiver(progressReciever, progress);
+        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(progressReciever, progress);
 
         IntentFilter close = new IntentFilter();
         close.addAction(Constants.ACTION_CLOSE_DIALOG);
-        registerReceiver(closeReciever, close);
+        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(closeReciever, close);
 
         IntentFilter genericMessage = new IntentFilter();
         genericMessage.addAction(Constants.GENERIC_DIALOG);
-        registerReceiver(genericMessageReciever, genericMessage);
+        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(genericMessageReciever, genericMessage);
 
         IntentFilter notRom = new IntentFilter();
         notRom.addAction(Constants.ACTION_NOT_ROM_DIALOG);
-        registerReceiver(notRomReciever, notRom);
+        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(notRomReciever, notRom);
         super.onResume();
     }
     @Override
@@ -137,11 +138,11 @@ public class DeltaDialogActivity extends Activity {
     }
     @Override
     public void onPause() {
-        unregisterReceiver(closeReciever);
-        unregisterReceiver(applyReciever);
-        unregisterReceiver(progressReciever);
-        unregisterReceiver(genericMessageReciever);
-        unregisterReceiver(notRomReciever);
+        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(closeReciever);
+        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(applyReciever);
+        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(progressReciever);
+        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(genericMessageReciever);
+        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(notRomReciever);
         super.onPause();
     }
     @Override

app/src/main/java/delta/out386/thugota/MainActivity.java
@@ -26,6 +26,7 @@ import android.app.FragmentManager;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 import android.content.Intent;
 import java.util.List;
@@ -36,7 +37,6 @@ import android.util.Log;
 public class MainActivity extends Activity
 implements NavigationDrawerFragment.NavigationDrawerCallbacks
 {
-	final String TAG = Constants.TAG;
     private NavigationDrawerFragment mNavigationDrawerFragment;
 
     @Override
@@ -106,15 +106,15 @@ implements NavigationDrawerFragment.NavigationDrawerCallbacks
         @Override
         protected Void doInBackground(Void... voids) {
             Intent notRomDialog = new Intent(Constants.ACTION_NOT_ROM_DIALOG);
-            startActivity(new Intent(getApplicationContext(), DeltaDialogActivity.class)
+            startActivity(new Intent(getApplication(), DeltaDialogActivity.class)
                     .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
             try {
                 Thread.sleep(4000);
-                // Delay needed as the activity needs time to register the reciever
+                // Delay needed as the activity needs time to register the receiver
             }
             catch (InterruptedException e)
             {}
-            sendBroadcast(notRomDialog);
+            LocalBroadcastManager.getInstance(getApplication()).sendBroadcast(notRomDialog);
             finish();
             return null;
         }

out386/xosp_delta

app/src/main/java/delta/out386/xosp/DeltaDialogActivity.java
@@ -25,6 +25,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
@@ -103,23 +104,23 @@ public class DeltaDialogActivity extends Activity {
     protected void onResume() {
         IntentFilter apply = new IntentFilter();
         apply.addAction(Constants.ACTION_APPLY_DIALOG);
-        registerReceiver(applyReciever, apply);
+        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(applyReciever, apply);
 
         IntentFilter progress = new IntentFilter();
         progress.addAction(Constants.PROGRESS_DIALOG);
-        registerReceiver(progressReciever, progress);
+        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(progressReciever, progress);
 
         IntentFilter close = new IntentFilter();
         close.addAction(Constants.ACTION_CLOSE_DIALOG);
-        registerReceiver(closeReciever, close);
+        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(closeReciever, close);
 
         IntentFilter genericMessage = new IntentFilter();
         genericMessage.addAction(Constants.GENERIC_DIALOG);
-        registerReceiver(genericMessageReciever, genericMessage);
+        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(genericMessageReciever, genericMessage);
 
         IntentFilter notXosp = new IntentFilter();
         notXosp.addAction(Constants.ACTION_NOT_XOSP_DIALOG);
-        registerReceiver(notXospReciever, notXosp);
+        LocalBroadcastManager.getInstance(getApplication()).registerReceiver(notXospReciever, notXosp);
         super.onResume();
     }
     @Override
@@ -138,11 +139,11 @@ public class DeltaDialogActivity extends Activity {
     }
     @Override
     public void onPause() {
-        unregisterReceiver(closeReciever);
-        unregisterReceiver(applyReciever);
-        unregisterReceiver(progressReciever);
-        unregisterReceiver(genericMessageReciever);
-        unregisterReceiver(notXospReciever);
+        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(closeReciever);
+        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(applyReciever);
+        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(progressReciever);
+        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(genericMessageReciever);
+        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(notXospReciever);
         super.onPause();
     }
     @Override

app/src/main/java/delta/out386/xosp/MainActivity.java
@@ -25,6 +25,7 @@ import android.app.FragmentManager;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 import android.content.Intent;
 import java.util.List;
@@ -35,7 +36,6 @@ import android.util.Log;
 public class MainActivity extends Activity
 implements NavigationDrawerFragment.NavigationDrawerCallbacks
 {
-	final String TAG = Constants.TAG;
     private NavigationDrawerFragment mNavigationDrawerFragment;
 
     @Override
@@ -100,15 +100,15 @@ implements NavigationDrawerFragment.NavigationDrawerCallbacks
         @Override
         protected Void doInBackground(Void... voids) {
             Intent notXospDialog = new Intent(Constants.ACTION_NOT_XOSP_DIALOG);
-            startActivity(new Intent(getApplicationContext(), DeltaDialogActivity.class)
+            startActivity(new Intent(getApplication(), DeltaDialogActivity.class)
                     .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
             try {
                 Thread.sleep(4000);
-                // Delay needed as the activity needs time to register the reciever
+                // Delay needed as the activity needs time to register the receiver
             }
             catch (InterruptedException e)
             {}
-            sendBroadcast(notXospDialog);
+            LocalBroadcastManager.getInstance(getApplication()).sendBroadcast(notXospDialog);
             finish();
             return null;
         }