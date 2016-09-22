79a0fd47b66835058a22d78bf5ac021ba6ee8c1c
Ritayan Chakraborty <ritayanout@gmail.com>
2016-09-22 10:52:02 +0530

    temp

--- app/src/main/java/com/grarak/kerneladiutor/fragments/RecoveryFragment.java
+++ app/src/main/java/com/grarak/kerneladiutor/fragments/RecoveryFragment.java
@@ -19,9 +19,16 @@
  */
 package com.grarak.kerneladiutor.fragments;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.ClipData;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.widget.CheckBox;
 import android.view.LayoutInflater;
@@ -31,13 +38,11 @@ import android.widget.LinearLayout;
 import android.graphics.Color;
 
 import android.widget.ListView;
-import com.github.developerpaul123.filepickerlibrary.enums.MimeType;
-import com.github.developerpaul123.filepickerlibrary.enums.Request;
-import com.github.developerpaul123.filepickerlibrary.enums.Scope;
 import com.grarak.kerneladiutor.utils.Prefs;
 import com.grarak.kerneladiutor.utils.Utils;
 import com.grarak.kerneladiutor.utils.root.RootFile;
 import com.grarak.kerneladiutor.utils.tools.Recovery;
+import com.nononsenseapps.filepicker.FilePickerActivity;
 
 import java.io.File;
 import java.io.IOException;
@@ -46,12 +51,8 @@ import java.util.List;
 
 import delta.out386.xosp.Constants;
 import delta.out386.xosp.R;
-import com.github.developerpaul123.filepickerlibrary.FilePickerActivity;
 import delta.out386.xosp.RecoveryAdapter;
 import delta.out386.xosp.SortFileType;
-
-import static android.app.Activity.RESULT_OK;
-import static com.github.developerpaul123.filepickerlibrary.FilePickerActivity.REQUEST_FILE;
 
 /**
  * Created by willi on 12.07.16.
@@ -61,6 +62,8 @@ public class RecoveryFragment extends Fragment {
     private List<Recovery> mCommands = new ArrayList<>();
     private int mRecoveryOption;
     private RecoveryAdapter adapter;
+    private String loc;
+    final private int FILE_CODE = 1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -89,7 +92,6 @@ public class RecoveryFragment extends Fragment {
                     for (int i = 0; i < checkBoxes.size(); i++) {
                         checkBoxes.get(i).setChecked(position == i);
                     }
-                    Log.i(Constants.TAG, "onClick: Saving Reco " + position);
                     Prefs.saveInt("recovery_option", position, getActivity());
                     mRecoveryOption = position;
                 }
@@ -97,14 +99,17 @@ public class RecoveryFragment extends Fragment {
 
             checkBoxes.add(checkBox);
             layout.addView(checkBox);
+            SharedPreferences preferences = getActivity().getSharedPreferences("settings", Context.MODE_PRIVATE);
+            String defaultDir = Environment.getExternalStorageDirectory().getAbsolutePath();
+            loc = preferences.getString("location", defaultDir);
         }
 
         rootView.findViewById(R.id.flash_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (mCommands != null && mCommands.size() > 0) {
-                    flashNow(mRecoveryOption);
-                    Utils.toast("Selected reco " + mRecoveryOption, getActivity());
+                    FlashNow flashNow = new FlashNow(mCommands, mRecoveryOption);
+                    flashNow.execute();
                 } else {
                     Utils.toast(R.string.add_action_first, getActivity());
                 }
@@ -120,19 +125,12 @@ public class RecoveryFragment extends Fragment {
     }
 
     private void add() {
-        /*Intent intent = new Intent(getActivity(), FilePickerActivity.class);
-        intent.putExtra(FilePickerActivity.PATH_INTENT, "/sdcard");
-        intent.putExtra(FilePickerActivity.EXTENSION_INTENT, ".zip");
-        startActivityForResult(intent, 0);*/
-        /*FilePickerBuilder.getInstance()
-                .setActivityTheme(R.style.AppTheme)
-                .pickDocument(RecoveryFragment.this)*/;//getActivity());
-        Intent filePicker = new Intent(getContext(), FilePickerActivity.class);
-        filePicker.putExtra(FilePickerActivity.SCOPE, Scope.ALL);
-        filePicker.putExtra(FilePickerActivity.REQUEST, Request.FILE);
-        filePicker.putExtra(FilePickerActivity.INTENT_EXTRA_COLOR_ID, getResources().getColor(R.color.colorPrimary));
-        filePicker.putExtra(FilePickerActivity.MIME_TYPE, MimeType.TXT);
-        startActivityForResult(filePicker, REQUEST_FILE);
+        Intent i = new Intent(getContext(), FilePickerActivity.class);
+        i.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, true);
+        i.putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, false);
+        i.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_FILE);
+        i.putExtra(FilePickerActivity.EXTRA_START_PATH, loc);
+        startActivityForResult(i, FILE_CODE);
     }
 
     private void addAction(Recovery.RECOVERY_COMMAND recovery_command, String path) {
@@ -153,51 +151,42 @@ public class RecoveryFragment extends Fragment {
         try {
             if(path != null)
             type = new SortFileType().sort(new File(path));
+            if(type != null && type.equals("noFlash")) {
+                Utils.toast(path + getResources().getString(R.string.not_flashable_zip), getActivity());
+                return;
+            }
         } catch(IOException e) {
-            Utils.toast("File " + path + " is not a zip", getActivity());
+            Utils.toast(path + getResources().getString(R.string.not_flashable_zip), getActivity());
             return;
         }
         final Recovery recovery = new Recovery(recovery_command, path == null ? null : new File(path), summary, type);
         mCommands.add(recovery);
         adapter.notifyDataSetChanged();
-        /*CardView cardView = new CardView(getActivity());
-        cardView.setOnMenuListener(new CardView.OnMenuListener() {
-            @Override
-            public void onMenuReady(final CardView cardView, PopupMenu popupMenu) {
-                popupMenu.getMenu().add(Menu.NONE, 0, Menu.NONE, getString(R.string.delete));
-                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        if (item.getItemId() == 0) {
-                            mCommands.remove(recovery);
-                            removeItem(cardView);
-                        }
-                        return false;
-                    }
-                });
-            }
-        });*/
-
-        /*DescriptionView descriptionView = new DescriptionView();
-        if (path != null) {
-            descriptionView.setTitle(getString(R.string.flash_zip));
-        }
-        descriptionView.setSummary(summary);
-
-        cardView.addItem(descriptionView);
-        addItem(cardView);*/
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if ((requestCode == REQUEST_FILE) && (resultCode == RESULT_OK)) {
-            addAction(Recovery.RECOVERY_COMMAND.FLASH_ZIP, data.getStringExtra(FilePickerActivity.FILE_EXTRA_DATA_PATH));
-            Utils.toast(data.getStringExtra(FilePickerActivity.FILE_EXTRA_DATA_PATH), getActivity());
+        if (requestCode == FILE_CODE && resultCode == Activity.RESULT_OK) {
+            if (data.getBooleanExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false)) {
+                ClipData clip = data.getClipData();
+
+                if (clip != null) {
+                    for (int i = 0; i < clip.getItemCount(); i++) {
+                        Uri uri = clip.getItemAt(i).getUri();
+                        addAction(Recovery.RECOVERY_COMMAND.FLASH_ZIP, uri.getPath());
+                        Utils.toast(uri.getPath(), getActivity());
+                    }
+                }
+            } else {
+                Uri uri = data.getData();
+                addAction(Recovery.RECOVERY_COMMAND.FLASH_ZIP, uri.getPath());
+                Utils.toast(uri.getPath(), getActivity());
+            }
         }
     }
 
-    private void flashNow(final int recoveryOption) {
+    /*private void flashNow(final int recoveryOption){
         String file = "/cache/recovery/" + mCommands.get(0).getFile(recoveryOption == 1 ?
                 Recovery.RECOVERY.TWRP : Recovery.RECOVERY.CWM);
         RootFile recoveryFile = new RootFile(file);
@@ -209,77 +198,16 @@ public class RecoveryFragment extends Fragment {
                 recoveryFile.write(command, true);
         }
         //Shell.SU.run("reboot recovery");
-    }
-
-
-    //public static class OptionsFragment extends BaseFragment {
-        /*public static OptionsFragment newInstance(RecoveryFragment recoveryFragment) {
-            OptionsFragment fragment = new OptionsFragment();
-            fragment.mRecoveryFragment = recoveryFragment;
-            return fragment;
-        }*/
-
-        /*private RecoveryFragment mRecoveryFragment;
-        private int mRecoveryOption;
-
-        @Nullable
-        @Override
-        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                                 @Nullable Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_recovery_options, container, false);*/
-            /*mRecoveryOption = Prefs.getInt("recovery_option", 0, getActivity());
-
-            LinearLayout layout = (LinearLayout) rootView.findViewById(R.id.layout);
-            String[] options = getResources().getStringArray(R.array.recovery_options);
-
-            final List<AppCompatCheckBox> checkBoxes = new ArrayList<>();
-            for (int i = 0; i < options.length; i++) {
-                AppCompatCheckBox checkBox = new AppCompatCheckBox(
-                        new ContextThemeWrapper(getActivity(),
-                                R.style.Base_Widget_AppCompat_CompoundButton_CheckBox_Custom), null, 0);
-                checkBox.setText(options[i]);
-                checkBox.setTextColor(Color.WHITE);
-                checkBox.setChecked(i == mRecoveryOption);
-                checkBox.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT));
-
-                final int position = i;
-                checkBox.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        for (int i = 0; i < checkBoxes.size(); i++) {
-                            checkBoxes.get(i).setChecked(position == i);
-                        }
-                        Prefs.saveInt("recovery_option", position, getActivity());
-                        mRecoveryOption = position;
-                    }
-                });
-
-                checkBoxes.add(checkBox);
-                layout.addView(checkBox);
-            }
-
-            rootView.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (mRecoveryFragment != null && mRecoveryFragment.itemsSize() > 0) {
-                        mRecoveryFragment.flashNow(mRecoveryOption);
-                    } else {
-                        Utils.toast(R.string.add_action_first, getActivity());
-                    }
-                }
-            });
-
-            rootView.findViewById(R.id.reboot_button).setOnClickListener(new View.OnClickListener() {
+    }*/
+            /*rootView.findViewById(R.id.reboot_button).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     if (mRecoveryFragment != null) {
                         mRecoveryFragment.reboot();
                     }
                 }
-            });*/
-
-           /* return rootView;
+            });
+             return rootView;
         }
     }*/
 
@@ -289,3 +217,30 @@ public class RecoveryFragment extends Fragment {
         mCommands.clear();
     }
 }
+    class FlashNow extends AsyncTask<Void,Void,Void> {
+        private List<Recovery> mCommands;
+        private int mRecoveryOption;
+        public FlashNow(List<Recovery> commands, int recoveryOption) {
+            mCommands = commands;
+            mRecoveryOption = recoveryOption;
+        }
+        @Override
+        public Void doInBackground(Void... p) {
+            String file = "/cache/recovery/" + mCommands.get(0).getFile(mRecoveryOption == 1 ?
+                    Recovery.RECOVERY.TWRP : Recovery.RECOVERY.CWM);
+            RootFile recoveryFile = new RootFile(file);
+            recoveryFile.delete();
+            for (Recovery commands : mCommands) {
+                for (String command : commands.getCommands(mRecoveryOption == 1 ?
+                        Recovery.RECOVERY.TWRP :
+                        Recovery.RECOVERY.CWM))
+                    recoveryFile.write(command, true);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void p) {
+            //Shell.SU.run("reboot recovery");
+        }
+    }
